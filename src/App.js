83abb0e2057eda112ca55e05f6d8a9632c8fe447
Taylor-Stacey <taylor88stacey@gmail.com>
2022-06-22 11:46:01 -0700

    mostly done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, link, Link } from 'react-router-dom';
+
+import Login from './components/Login';
+import FriendsList from './components/FriendsList';
+import AddFriend from './components/AddFriends';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Client Auth Project</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <h2>Friends Database</h2>
+          <Link className='link' to='login'>Login</Link>
+          <Link className='link' to='friends'>Friends List</Link>
+          <Link className='link' to='friends/add'>Add Friends</Link>
+          <Link className='link' to='friends'>Log Out</Link>
+        </header>
+        <Route exact path='/'>
+          <Login/>
+        </Route>
+
+        <Route exact path='/login'>
+          <Redirect to='/'/>
+        </Route>
+
+        <Route exact path='/friends'>
+          <FriendsList/>
+        </Route>
+
+        <Route exact path='/friends/add'>
+          <AddFriend/>
+        </Route>
+
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/FriendsList.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+const FriendsList = () => {
+    const [friends, setFriends] = useState([]);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        axios.get('http://localhost:3000/api/friends', {
+            headers:{
+                authorization: token
+            }
+        })
+        .then(res => {
+            setFriends(res.data)
+        })
+        .catch(err =>{
+            console.log(err);
+        });
+    }, []);
+
+    return (<div>
+        <h2>FriendsList</h2>
+        <ul>
+            {
+                friends.map(friend=> {
+                    return <li>{friend.name} - {friend.age} - {friend.email}</li>
+                })
+            }
+        </ul>
+    </div>
+    )
+}
+
+export default FriendsList;
